0dffbcd4ae334a946a25004b3e95703007e2ebed
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-06-01 17:21:12 +0900

    ユーザー情報の登録

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_POST['name']) && isset($_POST['mail'])){//新規ユーザ登録
+        try{
+                // 接続
+                $db = new PDO('mysql:host=192.168.0.159;dbname=HUG', 'miyashita', 'sonicdance');
+                $write=$db->prepare('INSERT INTO user (mail, name, role) VALUES(:mail, :name, :role)');
+                $write->bindvalue(':mail',$_POST['mail']);
+                $write->bindvalue(':name',$_POST['name']);
+                $write->bindvalue(':role',1);
+                $write->execute();
+                $db = null;// 切断
+        } catch(PDOException $e){ //データベース接続失敗
+            echo $e->getMessage();
+            exit;
+        }
+
+}
+
+        ?>
