6b0a56d6fffb445296c237843b18cf8f35b532ab
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-06-18 17:15:26 +0900

    受信データが登録済みのデータと一致するか確認してデータを送信

--- login.php
+++ login.php
@@ -1,28 +1,57 @@
 <?php
-if(isset($_POST['name']) && isset($_POST['mail']) && isset($_POST['role'])){//新規ユーザ登録
+if(isset($_POST['pass']) && isset($_POST['mail']) && isset($_POST['faze'])){//新規ユーザ登録
         try{
-                $cnt;
-                $mysqli = new mysqli( '192.168.0.159', 'miyashita', 'sonicdance', 'HUG');
-                if ($result = $mysqli->query("SELECT mail FROM user")) {
-
-                        $cnt = $result->num_rows;
-                        $result->close();
-                }
-                $mysqli->close();
-
                 // 接続
                 $db = new PDO('mysql:host=192.168.0.159;dbname=HUG', 'miyashita', 'sonicdance');
-                $write=$db->prepare('INSERT INTO user (mail, name, role, number) VALUES(:mail, :name, :role, :number)');
-                $write->bindvalue(':mail',$_POST['mail']);
-                $write->bindvalue(':name',$_POST['name']);
-                $write->bindvalue(':role',$_POST['role']);
-                $write->bindvalue(':number',$cnt + 1);
 
-                $write->execute();
-                echo json_encode($cnt + 1);
+                $pass = $_POST['pass'];
+                $mail = $_POST['mail'];
+
+                switch ($_POST['faze']) {
+                        case 'login':
+                                $sqldata = $db->prepare("SELECT mail, name FROM user WHERE '$pass' = user.pass AND user.mail = '$mail'");
+                                $sqldata->execute();
+                                $db_data = null;
+                                while ($row = $sqldata->fetch()) {
+                                        $db_data[] = array(
+                                                'mail'=>$row['mail'],
+                                                'name'=>$row['name']
+                                                );
+                                }
+                                echo json_encode($db_data);
+                                break;
+                        case 'new':
+                              
+                        break;
+
+                        case 'forget':
+                              
+                        break;
+
+                }
+                
+                // $cnt;
+                // $mysqli = new mysqli( '192.168.0.159', 'miyashita', 'sonicdance', 'HUG');
+                // if ($result = $mysqli->query("SELECT mail FROM user")) {
+
+                //         $cnt = $result->num_rows;
+                //         $result->close();
+                // }
+                // $mysqli->close();
+
+                // // 接続
+                // $db = new PDO('mysql:host=192.168.0.159;dbname=HUG', 'miyashita', 'sonicdance');
+                // $write=$db->prepare('INSERT INTO user (mail, name, role, number) VALUES(:mail, :name, :role, :number)');
+                // $write->bindvalue(':mail',$_POST['mail']);
+                // $write->bindvalue(':name',$_POST['name']);
+                // $write->bindvalue(':role',$_POST['role']);
+                // $write->bindvalue(':number',$cnt + 1);
+
+                // $write->execute();
+                // echo json_encode($cnt + 1);
                 $db = null;// 切断
         } catch(PDOException $e){ //データベース接続失敗
-            echo $e->getMessage();
+        //     echo $e->getMessage();
             exit;
         }
 }
