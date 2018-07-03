35d0b694d0dddaca1a834394c470ce64b75ace76
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-07-03 16:24:56 +0900

    テーマ一覧に必要なデータの読み込みと送信、テーマ詳細に必要なデータの読み込みと送信

--- theme.php
+++ theme.php
@@ -1,6 +1,16 @@
 
 <?php
-//if()
+$word = array('id', 'title', 'category', 'situation', 'time', 'area', 'image', 'target', 'aim', 'open', 'create_day', 'play_count', 'assessment0', 'assessment1', 'assessment2', 'assessment3', 'assessment4', 'assessment5', 'assessment6', 'assessment7', 'assessment8', 'assessment9');
+$f = 1;
+for($i = 1; $i < 4; $i++){
+        if(!(isset($_POST[$word[$i]])))
+                $f = 0;
+}
+if(!(isset($_POST[$word[9]])))
+        $f = 0;
+if(!(isset($_POST[$word[12]])))
+        $f = 0;
+if($f == 1)
         try {
                 $db =new PDO('mysql:host=192.168.0.159;dbname=HUG;','miyashita','sonicdance');
 
@@ -10,7 +20,6 @@
                 $stmt->execute();
                 $count=$stmt->rowCount();
 
-                $word = array('id', 'title', 'category', 'situation', 'time', 'area', 'image', 'target', 'aim', 'open', 'create_day', 'play_count', 'assessment0', 'assessment1', 'assessment2', 'assessment3', 'assessment4', 'assessment5', 'assessment6', 'assessment7', 'assessment8', 'assessment9');
                 $write=$db->prepare("INSERT INTO theme (id, title, category, situation, time, area, image, target, aim, open, create_day, play_count, assessment0, assessment1, assessment2, assessment3, assessment4, assessment5, assessment6, assessment7, assessment8, assessment9) VALUES(:id, :title,:category, :situation, :time, :area, :image, :target, :aim, :open, :create_day, :play_count, :assessment0, :assessment1, :assessment2, :assessment3, :assessment4, :assessment5, :assessment6, :assessment7, :assessment8, :assessment9)");
                 $write->bindvalue(":id", $count + 1);
                 for($i = 1; $i < count($word); $i++){
@@ -39,21 +48,66 @@
                 echo "EE";
 
         }
-// else
-//         try {
-//         $db =new PDO('mysql:host=192.168.0.159;dbname=HUG;','miyashita','sonicdance');
+else if(isset($_POST["mode"]))
+        try {
+                $db =new PDO('mysql:host=192.168.0.159;dbname=HUG;','miyashita','sonicdance');
 
-//         $sqldata = $db->prepare("SELECT word FROM master_category");
-//         $sqldata->execute();
-//         while ($row = $sqldata->fetch()) {
-//         $db_data[] = array(
-//                 'word'=>$row['word']
-//                 );
-//         }
-//         //JSONデータ出力
-//         header("Content-type: application/json; charset=UTF-8");
-//         echo json_encode($db_data);
-//         $db=null;
-//         } catch (Exception $e) {
-//         }
+                $sqldata = $db->prepare("SELECT id, title, category, create_day, play_count FROM theme
+                ");
+                $sqldata->execute();
+                while ($row = $sqldata->fetch()) {
+                        $db_data[] = array(
+                        'id'=>$row['id'],
+                        'title'=>$row['title'],
+                        'category'=>$row['category'],
+                        'create_day'=>$row['create_day'],
+                        'play_count'=>$row['play_count']
+                        );
+                }
+                //JSONデータ出力
+                header("Content-type: application/json; charset=UTF-8");
+                echo json_encode($db_data);
+                $db=null;
+        } catch (Exception $e) {
+        }
+else {
+        try {
+                $db =new PDO('mysql:host=192.168.0.159;dbname=HUG;','miyashita','sonicdance');
+
+                $sqldata = $db->prepare("SELECT id, title, category, situation, time, area, image, target, aim, open, create_day, play_count, assessment0, assessment1, assessment2, assessment3, assessment4, assessment5, assessment6, assessment7, assessment8, assessment9 FROM theme
+                ");
+                $sqldata->execute();
+                while ($row = $sqldata->fetch()) {
+                        $db_data[] = array(
+                        'id'=>$row['id'],
+                        'title'=>$row['title'],
+                        'category'=>$row['category'],
+                        'situation'=>$row['situation'],
+                        'time'=>$row['time'],
+                        'area'=>$row['area'],
+                        'image'=>$row['image'],
+                        'target'=>$row['target'],
+                        'aim'=>$row['aim'],
+                        'open'=>$row['open'],
+                        'create_day'=>$row['create_day'],
+                        'play_count'=>$row['play_count'],
+                        'assessment0'=>$row['assessment0'],
+                        'assessment1'=>$row['assessment1'],
+                        'assessment2'=>$row['assessment2'],
+                        'assessment3'=>$row['assessment3'],
+                        'assessment4'=>$row['assessment4'],
+                        'assessment5'=>$row['assessment5'],
+                        'assessment6'=>$row['assessment6'],
+                        'assessment7'=>$row['assessment7'],
+                        'assessment8'=>$row['assessment8'],
+                        'assessment9'=>$row['assessment9'],
+                );
+                }
+                //JSONデータ出力
+                header("Content-type: application/json; charset=UTF-8");
+                echo json_encode($db_data);
+                $db=null;
+        } catch (Exception $e) {
+        }
+}
 ?>
