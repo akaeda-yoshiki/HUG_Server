405bd7ffcd34a97166d068d09f72cb66030132f8
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-07-06 15:47:51 +0900

    画像名を「テーマのid .jpeg」に設定

--- file_upload.php
+++ file_upload.php
@@ -17,24 +17,28 @@
 // $url = $_POST["file"];
 // $data = file_get_contents($url);
 // file_put_contents('image/test.PNG', $url);
-if(strlen($_FILES["pic"]["name"])>0)
-{
-     $filename=$_FILES["pic"]["name"];
-     if(! move_uploaded_file($_FILES["pic"]["tmp_name"],"image/te.png") )
-     {
-          print "アップロードに失敗しました<br>";
-     }
-     else
-     {
-          print "name=";
-          print $filename;
-          print "<BR>";
-          print "<IMG src='$filename'>";
-          print "<BR>";
-     }
-}
-else
-{
-     print "ファイルをアプロードしてください";
+if (strlen($_FILES["pic"]["name"]) > 0 ) {
+        try {
+                $db = new PDO('mysql:host=192.168.0.159;dbname=HUG;', 'miyashita', 'sonicdance');
+
+                $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+                $sql = 'SELECT * FROM theme';
+                $stmt = $db->query($sql);
+                $stmt->execute();
+                $count = $stmt->rowCount();
+                $filename = $_FILES["pic"]["name"];
+                if (!move_uploaded_file($_FILES["pic"]["tmp_name"], "image/" . $count . ".jpeg")) {
+                        print "アップロードに失敗しました<br>";
+                } else {
+                        print "name=";
+                        print $filename;
+                        print "<BR>";
+                        print "<IMG src='$filename'>";
+                        print "<BR>";
+                }
+                $db = null;
+        } catch (Exception $e) {
+
+        }
 }
 ?>
