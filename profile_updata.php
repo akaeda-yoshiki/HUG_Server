366e3a5e64429fa86f58a259d2fbd8a5bbc12e21
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-07-12 12:03:24 +0900

    プロフィール更新時にパスワードが登録されているものと一致しているかの確認を実装

--- profile_updata.php
+++ profile_updata.php
@@ -3,12 +3,19 @@ if (isset($_POST['mail'])) {
         try {
                 // 接続
                 $db = new PDO('mysql:host=192.168.0.159;dbname=HUG', 'miyashita', 'sonicdance');
-
                 $mail = $_POST['mail'];
-
-                if (isset($_POST['pass']))
-                        if (!(empty($_POST['pass']))) {
-                        $pass = $_POST['pass'];
+                $sqldata = $db->prepare("SELECT pass FROM user WHERE user.mail = '$mail'");
+                $sqldata->execute();
+                while ($row = $sqldata->fetch()) {
+                        $db_data[] = array(
+                                'pass' => $row['pass']
+                        );
+                }
+                $now_pass = $db_data[0]['pass'];
+                if (isset($_POST['new_pass']) && isset($_POST['now_pass']))
+                        if (strcmp($_POST['now_pass'], $now_pass) == 0)
+                        if (!(empty($_POST['new_pass']))) {
+                        $pass = $_POST['new_pass'];
                         $sqldata = $db->prepare("UPDATE user SET pass = '$pass' WHERE user.mail = '$mail'");
                         $sqldata->execute();
                 }
@@ -17,12 +24,15 @@ if (isset($_POST['mail'])) {
                         $name = $_POST['name'];
                         $sqldata = $db->prepare("UPDATE user SET name = '$name' WHERE user.mail = '$mail'");
                         $sqldata->execute();
-
                 }
 
 
 
+
+
+
                 $db = null;// 切断
+
         } catch (PDOException $e) { //データベース接続失敗
         //     exit;
         }
