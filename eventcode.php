0557c6d3a45b8beaf50eac0e34acfbce3223df30
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-07-24 16:38:25 +0900

    イベントコードが以前のものと重複がなければ保存

--- eventcode.php
+++ eventcode.php
@@ -1,32 +1,56 @@
 <?php
 
 if (isset($_POST['theme_id']) && isset($_POST['mail']) && isset($_POST['open'])) {
-        $loop = 1;
-        while ($loop) {
+        try {
+                $loop = true;
+                while ($loop) {
         //ランダムな英数字の生成
-                $text = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPUQRSTUVWXYZ';
-                $code = substr(
-                        str_shuffle($text),
-                        0,
-                        10
-                );
+                        $text = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPUQRSTUVWXYZ';
+                        $code = substr(
+                                str_shuffle($text),
+                                0,
+                                10
+                        );
 
         // echo $code;
 
 
-                $db = new PDO('mysql:host=192.168.0.159;dbname=HUG;', 'miyashita', 'sonicdance');
-                $sqldata = $db->prepare("SELECT code FROM eventcode WHERE eventcode.code = '$code'");
-                $sqldata->execute();
-                while ($row = $sqldata->fetch()) {
-                        $db_data[] = array(
-                                'code' => $row['code']
-                        );
+                        $db = new PDO('mysql:host=192.168.0.159;dbname=HUG;', 'miyashita', 'sonicdance');
+                        $sqldata = $db->prepare("SELECT code FROM eventcode WHERE eventcode.code = '$code'");
+                        $sqldata->execute();
+                        while ($row = $sqldata->fetch()) {
+                                $db_data[] = array(
+                                        'code' => $row['code']
+                                );
+                        }
+
+                        if (empty($db_data)) {
+                        // 挿入***********************************************
+                                $loop = false;
+                        }
                 }
 
-                if (empty($$db_data)) {
-                        // 挿入***********************************************
-                        $loop = 0;
-                }
+                $write = $db->prepare('INSERT INTO eventcode (code, theme_id, mail, open) VALUES(:code, :theme_id, :mail, :open)');
+                $write->bindvalue(':code', $code);
+                $write->bindvalue(':theme_id', $_POST['theme_id']);
+                $write->bindvalue(':mail', $_POST['mail']);
+                $write->bindvalue(':open', $_POST['open']);
+                $write->execute();
+
+        // SQL作成
+                $sql = "CREATE TABLE '$code' (
+		id INT(11) AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(20),
+		age INT(11),
+		registry_datetime DATETIME
+	) engine=innodb default charset=utf8";
+
+	// SQL実行
+                $res = $db->query($sql);
+                $db = null;// 切断
+        } catch (PDOException $e) { //データベース接続失敗
+//     echo $e->getMessage();
+                exit;
         }
 }
 ?>
