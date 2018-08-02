44e300b9fec165db161432dc977e325a3cf3c5f6
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-08-02 23:06:20 +0900

    待機画面作成（途中）

--- eventcode.php
+++ eventcode.php
@@ -58,7 +58,7 @@ if (isset($_POST['theme_id']) && isset($_POST['mail']) && isset($_POST['open']))
                 $role = $_POST['role'];
 
                 $db = new PDO('mysql:host=192.168.0.159;dbname=HUG;', 'miyashita', 'sonicdance');
-                $sqldata = $db->prepare("SELECT id FROM aaa WHERE data2 = '$role'");
+                $sqldata = $db->prepare("SELECT id FROM `{$code}` WHERE data2 = '$role'");
                 $sqldata->execute();
                 while ($row = $sqldata->fetch()) {
                         $db_data[] = array(
@@ -79,9 +79,65 @@ if (isset($_POST['theme_id']) && isset($_POST['mail']) && isset($_POST['open']))
                         $write->execute();
                         echo "ok";
                 }
+                $db = null;
         } catch (PDOException $e) { //データベース接続失敗
         //     echo $e->getMessage();
                 exit;
+        }
+} else if (isset($_POST['code'])) {
+        try {
+                $code = $_POST['code'];
+
+                $db = new PDO('mysql:host=192.168.0.159;dbname=HUG;', 'miyashita', 'sonicdance');
+                $sqldata = $db->prepare("SELECT theme_id FROM eventcode WHERE eventcode.code = '$code'");
+                $sqldata->execute();
+                while ($row = $sqldata->fetch()) {
+                        $db_data[] = array(
+                                'theme_id' => $row['theme_id']
+                        );
+                }
+                $id = $db_data[0]["theme_id"];
+                // echo $id;
+                // if(empty($db_data)){
+                //         echo("oo");
+                // }
+                // else{
+                //         echo("pp");
+                // }
+                $sqldata = $db->prepare("SELECT id, title, category, situation, time, area, image, target, aim, open, create_day, play_count, assessment0, assessment1, assessment2, assessment3, assessment4, assessment5, assessment6, assessment7, assessment8, assessment9 FROM theme WHERE theme.id = '$id'");
+                $sqldata->execute();
+                while ($row = $sqldata->fetch()) {
+                        $db_data[] = array(
+                                'id' => $row['id'],
+                                'title' => $row['title'],
+                                'category' => $row['category'],
+                                'situation' => $row['situation'],
+                                'time' => $row['time'],
+                                'area' => $row['area'],
+                                'image' => $row['image'],
+                                'target' => $row['target'],
+                                'aim' => $row['aim'],
+                                'open' => $row['open'],
+                                'create_day' => $row['create_day'],
+                                'play_count' => $row['play_count'],
+                                'assessment0' => $row['assessment0'],
+                                'assessment1' => $row['assessment1'],
+                                'assessment2' => $row['assessment2'],
+                                'assessment3' => $row['assessment3'],
+                                'assessment4' => $row['assessment4'],
+                                'assessment5' => $row['assessment5'],
+                                'assessment6' => $row['assessment6'],
+                                'assessment7' => $row['assessment7'],
+                                'assessment8' => $row['assessment8'],
+                                'assessment9' => $row['assessment9'],
+                        );
+                }
+                //JSONデータ出力
+                header("Content-type: application/json; charset=UTF-8");
+                echo json_encode($db_data);
+                $db = null;
+        } catch (PDOException $e) { //データベース接続失敗
+        //     echo $e->getMessage();
         }
 }
 
